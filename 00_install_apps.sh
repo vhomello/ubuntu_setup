35aab0bc7a6b63da65aa1c1feac3099959350aab
Vitor Mello <vhomello@gmail.com>
2024-02-05 22:29:08 -0300

    add first setup

---
+++ 00_install_apps.sh
@@ -0,0 +1,8 @@
+# chmod +x install_apps.sh 
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install vim
+sudo apt-get -y install git-all
+sudo apt-get -y install python3-pip
+
---
+++ 01_github_ssh_setup.sh
@@ -0,0 +1,4 @@
+# chmod +x github_ssh_setup.sh
+
+ssh-keygen -t rsa -b 4096 -C "vhomello@gmail.com"
+cat ~/.ssh/id_rsa.pub
